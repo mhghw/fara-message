1b630dee20ecd02fd0858ba0fddd14d81c35b615
yeganeh <yeganeh.rad434@gmail.com>
2024-02-18 22:24:12 +0330

    corrected the editUser function

--- handler.go
+++ handler.go
@@ -3,47 +3,44 @@ package editUser
 import (
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/mhghw/fara-message/db"
 )
 
-// suppose users post their usernames to edit information
 func editUser(c *gin.Context) {
 	var username string
+
 	err := c.BindJSON(&username)
 	if err != nil {
 		fmt.Errorf("error binding JSON:%w", err)
 		c.Status(400)
 		return
 	}
+
 	if len(username) < 3 {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "the username or password is incorrect",
 		})
 		return
 	}
-	for i := 10001; i <= 10004; i++ {
-		temp := strconv.Itoa(i)
-		currentUser, err := db.UsersDB.GetUser(temp)
-		if err != nil {
-			fmt.Errorf("error:%w", err)
-			return
-		}
-		if currentUser.Username == username {
-			fmt.Println("enter new firstname:")
-			fmt.Scan(&currentUser.FirstName)
-			fmt.Println("enter new lastname:")
-			fmt.Scan(&currentUser.LastName)
-			err:=db.UsersDB.UpdateUser(currentUser)
-			if err!=nil {
-				fmt.Errorf("error:%w",err)
-				return
-			}
-			c.JSON(http.StatusOK, gin.H{
-				"message": "firstname and lastname edit successfully",
-			})
-		}
+
+	userUnderReview, err1 := db.UsersDB.GetUserByUsername(username)
+	if err1 != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "the username is incorrect",
+		})
 	}
+	fmt.Println("enter new firstname:")
+	fmt.Scan(&userUnderReview.FirstName)
+	fmt.Println("enter new lastname:")
+	fmt.Scan(&userUnderReview.LastName)
+	err2 := db.UsersDB.UpdateUser(userUnderReview)
+	if err2 != nil {
+		fmt.Errorf("error:%w", err2)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "firstname and lastname edited successfully",
+	})
 }
