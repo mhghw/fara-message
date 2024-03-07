ef09bb793e18d0a3805bc3e15140dad803a3fec0
Yeganeh <yeganeh.rad434@gmail.com>
2024-03-07 20:39:15 +0330

    add chat-id field

--- message.sql
+++ message.sql
@@ -1,7 +1,8 @@
 CREATE TABLE messages.message {
-    message_id int NOT NULL AUTO_INCREMENT,
+    ID int NOT NULL AUTO_INCREMENT,
     sender_id int NOT NULL,
     receiver_id int NOT NULL,
+    chat-id int NOT NULL,
     content LONGTEXT NOT NULL,
     FOREIGN KEY (sender_id) REFERENCES user(User_ID)  
     FOREIGN KEY (message_id) REFERENCES user(User_ID)
