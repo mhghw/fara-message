00b38d4ecf5f00ff340bb993082979c8b6198c28
Yeganeh <yeganeh.rad434@gmail.com>
2024-04-22 19:51:54 +0330

    remove delMesWebserver file

--- api/webserver.go
+++ api/webserver.go
@@ -10,16 +10,17 @@ import (
 func RunWebServer(port int) error {
 	addr := fmt.Sprintf(":%d", port)
 	router := gin.New()
+	router.POST("/user/edit",editUser)
 	router.POST("/user/register", Register)
 	router.POST("/user/change_password",changePassword)
 	router.POST("/login", login)
 	router.POST("/login", login)
-	router.POST("/user/edit",editUser) 
 	router.Use(AuthMiddleware)
 	router.POST("/new_direct_chat", NewDirectChat)
 	router.POST("/new_group_chat", NewGroupChat)
 	router.GET("/chat/:id/messages", GetChatMessages)
 	router.POST("/send/message", send_message.SendMessage)
+	router.POST("/delete/message", deleteMessage)
 	err := router.Run(addr)
 	return err
 }
